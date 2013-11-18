d012a0f39aebc29030f8ad028dac1cdea3e0bfe8
Toofifty <amatho250@gmail.com>
2013-11-18 19:37:42 +1100

    Work on map loading

--- Tittle/core/maploader.py
+++ Tittle/core/maploader.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+import base
+
+from base import *
+
+class loadMap(object):
+    def __init__(self, type,):
+        map_array = readTxt()
+    
+    def loadMap(name):
+        pass
+    
